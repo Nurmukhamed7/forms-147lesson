01bc8841ba2da037d8beb4daa410c69ca194d68f
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-10-20 18:50:42 +0200

    Initial commit

---
+++ src/components/RatingControl.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul>
+    <li :class="{ active: modelValue === 'poor' }">
+      <button type="button" @click="activate('poor')">Poor</button>
+    </li>
+    <li :class="{ active: modelValue === 'average' }">
+      <button type="button" @click="activate('average')">Average</button>
+    </li>
+    <li :class="{ active: modelValue === 'great' }">
+      <button type="button" @click="activate('great')">Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+
+  methods: {
+    activate(option) {
+      this.$emit('update:modelValue', option);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: rgb(254, 5, 254);
+}
+
+.active button {
+  border-color: rgb(254, 5, 254);
+}
+/* // modelValue === 'poor'
+// props === modelValue
+// v-model === rating
+// rating === 'poor' 
+	modelValue это и есть rating
+
+*/
+</style>
